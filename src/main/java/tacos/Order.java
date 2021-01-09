a1c83e54fd4a731f9db3507da0c4d936573cfc5e
daisong yu <declanscale@gmail.com>
2021-01-09 12:59:38 +0800

    added validator in order

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,19 +1,35 @@
 package tacos;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class Order {
 	
+	@NotBlank(message="Name is required")
 	private String name;
+	
+	@NotBlank(message="Street is required")
 	private String street;
+	
+	@NotBlank(message="City is required")
 	private String city;
+	
+	@NotBlank(message="state is required")
 	private String state;
+	
+	@NotBlank(message="zip code is required")
 	private String zip;
+	
+	@CreditCardNumber(message="not a valid credit card number")
 	private String ccNumber;
+	
+	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",message="must be formatted MM/YY")
 	private String ccExpiration;
+	
+	@Digits(integer=3,fraction=0,message="invalid CVV")
 	private String ccCVV;
 
 }
